2c85bbab6a62f0e379d14be530f2b51487e84d17
Eranda2001 <121021896+Eranda2001@users.noreply.github.com>
2023-07-24 21:00:32 +0530

    Add files via upload

---
+++ 21002126_Tutorial6/Ceaser.scala
@@ -0,0 +1,37 @@
+object Ceaser {
+  def encryption(str: String, shift: Int): String = {
+    val text = new StringBuilder //StringBuilder is a library used to append characters without creating objects at each instance
+
+    for (cur <- str) {
+      if (cur.isLetter) {
+        val base = if (cur.isUpper) 'A' else 'a'
+        val encryptedChar = ((cur - base + shift) % 26 + base).toChar
+        text.append(encryptedChar)
+      } else {
+        text.append(cur)
+      }
+    }
+    text.toString
+  }
+
+  def decryption(text: String, shift: Int): String = {
+    encryption(text, 26-shift)
+  }
+
+  def cipher(data: String, shift: Int, encrypt_decrypt: (String, Int) => String): String = {
+    encrypt_decrypt(data, shift)
+  }
+
+  def main(args: Array[String]): Unit = {
+    print("Enter the string to decrypt: ")
+    val str = scala.io.StdIn.readLine()
+    print("Enter the amount to switch: ")
+    val shift = scala.io.StdIn.readInt()
+
+    val encrypted = cipher(str, shift, encryption)
+    println("Encrypted String:"+ encrypted)
+
+    val decrypted = cipher(encrypted, shift, decryption)
+    println("Decrypted String: "+ decrypted)
+  }
+}
